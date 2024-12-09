be481e213c37281fd0293920384b34c69061dfeb
wendelluan <wendelluan@alu.ufc.br>
2024-12-09 13:40:19 -0300

    feat: Add Delete and Update routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaksDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,8 +13,27 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(@Body() createTaksDto: CreateTaksDto): Task {
     return this.tasksService.createTask(createTaksDto);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch('/:id')
+  updateTaskById(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Task {
+    const { title, description, status } = updateTaskDto;
+    return this.tasksService.updateTaskById(id, title, description, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaksDto } from './dto/create-task.dto';
+
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return task;
   }
 
   createTask(createTaksDto: CreateTaksDto): Task {
@@ -21,6 +30,31 @@ export class TasksService {
     };
 
     this.tasks.push(task);
+    return task;
+  }
+
+  deleteTaskById(id: string): void {
+    const index = this.tasks.findIndex((task) => task.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    this.tasks.splice(index, 1);
+  }
+
+  updateTaskById(
+    id: string,
+    title?: string,
+    description?: string,
+    status?: TaskStatus,
+  ): Task {
+    const task = this.getTaskById(id);
+
+    if (title) task.title = title;
+    if (description) task.description = description;
+    if (status) task.status = status;
+
     return task;
   }
 }
