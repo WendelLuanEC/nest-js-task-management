ed9053d917b862400baa371742aa5fa77b3cf9e8
wendelluan <wendelluan@alu.ufc.br>
2024-12-19 15:14:03 -0300

    feat: Add deleteTask with ORM

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaksDto } from './dto/create-task.dto';
 import { Task } from './task.entity';
 import { TasksRepository } from './tasks.repository';
+import { TaskStatus } from './task-status.enum';
 
 @Injectable()
 export class TasksService {
@@ -21,11 +22,19 @@ export class TasksService {
     return this.tasksRepository.createTask(createTaksDto);
   }
 
-  async deleteTask(id: string): Promise<void>{
+  async deleteTask(id: string): Promise<void> {
     const result = await this.tasksRepository.delete(id);
 
-    if(result.affected === 0){
+    if (result.affected === 0) {
       throw new NotFoundException(`Task with ID ${id} not found`);
     }
+  }
+
+  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+    const task = await this.tasksRepository.findOne({ where: { id } });
+
+    task.status = status;
+    await this.tasksRepository.save(task);
+    return task;
   }
 }
