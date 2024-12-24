675c850803e63023b895e6b78d30be33407dc7f9
wendelluan <wendelluan@alu.ufc.br>
2024-12-24 14:31:37 -0300

    feat: Add @Guard in tasks routes

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 import { TasksRepository } from './tasks.repository';
 import { TasksController } from './tasks.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   providers: [TasksService, TasksRepository],
   controllers: [TasksController],
   exports: [TasksRepository],
