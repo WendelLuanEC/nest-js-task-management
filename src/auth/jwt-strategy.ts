de0a813c9dff4578dea637f4deb9d0b91459dfb6
wendelluan <wendelluan@alu.ufc.br>
2024-12-25 14:57:42 -0300

    feat: JWT Secret config

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -5,15 +5,18 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersRepository } from './users.repository';
 import { JwtPayload } from './dto/jwt-payload.interface';
 import { User } from './user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET'),
+      //secretOrKey: 'topSecret51',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
