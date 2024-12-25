15c8023cb1498885512923e3e4c39960e3c3f115
wendelluan <wendelluan@alu.ufc.br>
2024-12-25 11:25:18 -0300

    feat: Setting up ConfigModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,9 +5,10 @@ import { TasksService } from './tasks.service';
 import { TasksRepository } from './tasks.repository';
 import { TasksController } from './tasks.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule, ConfigModule],
   providers: [TasksService, TasksRepository],
   controllers: [TasksController],
   exports: [TasksRepository],
